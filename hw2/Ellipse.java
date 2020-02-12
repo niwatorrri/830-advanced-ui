e84906e71a7c773fd6d95bd00c3572a4fd759c64
Niwatori <niwatori@foxmail.com>
2020-02-12 01:04:23 -0500

    implement Ellipse class

---
+++ hw2/Ellipse.java
@@ -0,0 +1,115 @@
+import java.awt.*;
+
+public class Ellipse implements GraphicalObject {
+    private int x, y, width, height;
+    private Color color;
+    private int lineThickness;
+    private Group group = null;
+
+    /**
+     * Constructors
+     */
+    public Ellipse(int x, int y, int width, int height,
+            Color color, int lineThickness) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.lineThickness = lineThickness;
+    }
+
+    public Ellipse() {
+        this(0, 0, 10, 10, Color.BLACK, 1);
+    }
+
+    /**
+     * Getters and setters
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getLineThickness() {
+        return this.lineThickness;
+    }
+
+    public void setLineThickness(int lineThickness) {
+        this.lineThickness = lineThickness;
+    }
+
+    /**
+     * Methods defined in the GraphicalObject interface
+     */
+    public void draw(Graphics2D graphics, Shape clipShape) {
+        Shape oldClip = graphics.getClip();
+        graphics.setClip(clipShape);
+
+        graphics.setColor(color);
+        graphics.setStroke(new BasicStroke(lineThickness));
+        graphics.drawOval(
+            x + lineThickness / 2,
+            y + lineThickness / 2,
+            width - lineThickness,
+            height - lineThickness
+        );
+        graphics.setClip(oldClip);
+    }
+
+    public BoundaryRectangle getBoundingBox() {
+        return new BoundaryRectangle(x, y, width, height);
+    }
+
+    public void moveTo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Group getGroup() {
+        return this.group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public boolean contains(int x, int y) {
+        return getBoundingBox().contains(x, y);
+    }
+}
