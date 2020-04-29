94a625f237eaac705978ff952b365c6cf6e02697
mcao2 <mcao2@andrew.cmu.edu>
2020-04-28 23:06:40 -0400

    remove previous workaround

--- talk-ui-project/src/main/java/ui/talk/TalkUI.java
+++ talk-ui-project/src/main/java/ui/talk/TalkUI.java
@@ -143,6 +143,8 @@ public class TalkUI extends InteractiveWindowGroup {
                 try {
                     target[0] = getSelection().get(0);
                     System.out.println("found target: " + target[0]);
+                    // unregister the behavior from the drawing canvas
+                    // drawingPanel.removeBehavior(this);
                 } catch (Exception e) {
                     target[0] = null;
                     System.out.println("no target found");
@@ -154,11 +156,6 @@ public class TalkUI extends InteractiveWindowGroup {
 
         // add a choice behavior to the drawing canvas to locate the target object
         drawingPanel.addBehavior(cBehavior);
-        addBehavior(cBehavior);
-
-        // unregister the behavior from the drawing canvas
-        // TODO: recover this after the bug fixed
-        // drawingPanel.removeBehavior(cBehavior);
 
         return Pair.of(target[0], Pair.of(startEvent, stopEvent));
     }
